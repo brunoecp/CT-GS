14baa12ed9ca73034e53d347db43749c5ba43d7f
brunoecp <rm95315@fiap.com.br>
2022-11-01 22:00:12 -0300

    top

--- GS2.py
+++ GS2.py
@@ -13,6 +13,13 @@ def cadastrar(tipousuario, nome, senha):
         listaUsuarios["usuarioSenha"].append(senha)
 def erroOpcao():
     print("Você inseriu algo estranho na opção, favor selecionar uma das opções oferecidas")
+def excluirCarro(id):
+    indice = 0
+    for i in range(len(listaCarro["id"])):
+        if id == listaCarro['id'][i]:
+            indice = i
+    for i in listaCarro:
+        listaCarro[i].pop(indice)
 def logar(tipo, nome, senha):
     if tipo == 'empresa':
         if(verificarLogin):
@@ -41,7 +48,13 @@ def logar(tipo, nome, senha):
 
 def excessederOpcao():
     print("Favor selecionar uma das opções oferecidas")
-def adicionarCarro(marca,ano,modelo,qntdPassageiros,placa,tipo,usuario):
+def contratarCarro(id):
+    indice = 0
+    for i in range(len(listaCarro["id"])):
+        if id == listaCarro['id'][i]:
+            indice = i
+    listaCarro["alugado"][indice] = True
+def adicionarCarro(marca,ano,modelo,qntdPassageiros,placa,tipo,id,usuario):
     listaCarro["marca"].append(marca)
     listaCarro["ano"].append(ano)
     listaCarro["modelo"].append(modelo)
@@ -49,10 +62,11 @@ def adicionarCarro(marca,ano,modelo,qntdPassageiros,placa,tipo,usuario):
     listaCarro["placa"].append(placa)
     listaCarro["tipo"].append(tipo)
     listaCarro["id"].append(id)
-    listaCarro["dono"].append(usuario)    
+    listaCarro["dono"].append(usuario)
+    listaCarro["alugado"].append(False)    
 listaUsuarios = {"usuario": ["empresa"], "usuarioSenha" : ["empresao"]}
 listaEmpresa = {"empresa" : ["usuario"], "empresaSenha" : ["usuarao"]}
-listaCarro = {"marca" : [], "placa": [], "modelo": [], "ano": [], "tipo": [], "qntdPassageiros": [], "id" : [], "dono": []}
+listaCarro = {"id" : [],"marca" : [], "placa": [], "modelo": [], "ano": [], "tipo": [], "qntdPassageiros": [] , "dono": [], "alugado": []}
 usuario = ""
 pula = ""
 id = 0
@@ -62,15 +76,15 @@ while(True):
         try:
             if usuario in listaEmpresa["empresa"]:
                 opcaoLogado()
-                opcao = int(input(f"1-adicionar Carro\n2-Mostrar seus carros\n3-LogOut\n: "))
+                opcao = int(input(f"1-adicionar Carro\n2-Mostrar seus carros\n3-Apagar Carro\n4-LogOut\n: "))
                 if opcao == 1:
                     try:
-                        marca = input("qual a marca?")
-                        ano = int(input("qual o ano?"))
-                        modelo = input("qual o modelo?")
-                        qntdPassageiros = int(input("quantas pessoas cabem?"))
-                        placa = input("qual a placa?")
-                        tipo = input("qual o tipo do carro(ex: SUV)?")
+                        marca = input("qual a marca?: ")
+                        ano = int(input("qual o ano?: "))
+                        modelo = input("qual o modelo?: ")
+                        qntdPassageiros = int(input("quantas pessoas cabem?: "))
+                        placa = input("qual a placa?: ")
+                        tipo = input("qual o tipo do carro(ex: SUV)?: ")
                         confirmarInfos= input(f"{pula:_^30}\nessas são as informações do carros?\nmarca: {marca}, ano: {ano}, modelo : {modelo}, quantidade de passageiros: {qntdPassageiros}, placa: {placa}, tipo: {tipo}\nse sim digite 's', se não, insira qualquer outra tecla").lower()
                         if(confirmarInfos == 's'):
                             adicionarCarro(marca,ano,modelo,qntdPassageiros,placa,tipo,id,usuario)
@@ -79,23 +93,48 @@ while(True):
                     except:
                         print("As informações não foram colocadas de maneira correta.")
                 elif opcao == 2:
-                    for i in range(listaCarro["dono"]):
+                    for i in range(len(listaCarro["dono"])):
                         if listaCarro["dono"][i] == usuario:
-                            print(f'{listaCarro["id"]} | {listaCarro["modelo"][i]} | {listaCarro["modelo"][i]}')
+                            print(f'{listaCarro["id"][i]} | {listaCarro["marca"][i]} | {listaCarro["modelo"][i]}')
                 elif opcao == 3:
+                    excluir = int(input("Qual o ID do carro que você quer excluir?(caso não saiba, saia digitando '-1', e veja na opcao 2)\n: "))
+                    if excluir == -1:
+                        continue
+                    elif excluir in listaCarro["id"]:
+                        excluirCarro(excluir)
+                    else: 
+                        print(f"{pula:_^30}\nid não encontrado") 
+                elif opcao == 4:
                     usuario = ""
                     continue
+                else:
+                    excessederOpcao()
             elif usuario in listaUsuarios["usuario"]:
                 opcaoLogado()
-                opcao = int(input(f"1-ver carros disponiveis\n3-LogOut\n:"))
+                opcao = int(input(f"1-ver carros disponiveis\n2-Contratar Carro\n3-LogOut\n:"))
                 if opcao ==1:
-                    print(listaCarro)
+                    for i in range(len(listaCarro)):
+                        if listaCarro["alugado"][i] is False:
+                            print(f"carro {listaCarro['id'][i] + 1}")
+                            for j in listaCarro:
+                                print(f'{pula:_^30}\n{j} : {listaCarro[j][i]}')
+                elif opcao == 2:
+                    contratar = int(input("Qual o id do carro que deseja contratar?(caso não saiba digite -1 e veja na primeira opção)"))
+                    if contratar == -1:
+                        continue
+                    elif contratar in listaCarro["id"]:
+                        contratarCarro(contratar)
+                    else: 
+                        print(f"{pula:_^30}\nid não encontrado") 
                 elif opcao == 3: 
                     usuario = ""
                     continue
+                else:
+                    excessederOpcao()
         except:
             print(f"{pula:_^30}\nVocê inseriu algo estranho na opção, favor selecionar uma das opções oferecidas")
     else:
+        try:
             print(usuario)
             print(f"{pula:_^30}\nVocê não está logado, deseja: ")
             opcao = int(input("1-Cadastrar-se\n2-logar-me\n3-sair\n : "))
@@ -145,4 +184,6 @@ while(True):
                 break
             else:
                 excessederOpcao()
+        except:
+            erroOpcao()
     
